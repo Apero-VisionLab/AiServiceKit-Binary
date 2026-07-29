555c05ff4102ddfefd4987b9a264dbcd4587289f
duypd2915 <duypd@apero.vn>
2026-07-29 17:12:54 +0700

    release 2.3.5

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AiServiceKit",
-            url: "https://github.com/AperoVN/AiServiceKit-Binary/releases/download/2.3.4/AiServiceKit.xcframework.zip",
-            checksum: "ba641195f83be1b23166122e141f3c5fccfe27c74c4f8cc2260de531ddd1914e"
+            url: "https://github.com/AperoVN/AiServiceKit-Binary/releases/download/2.3.5/AiServiceKit.xcframework.zip",
+            checksum: "8355ca3e52898168fd11a9c24c65808d2581004d2b6be553640008a2fc694f4f"
         )
     ]
 )
